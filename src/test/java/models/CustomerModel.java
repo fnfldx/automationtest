48bf986573451417800df942ce00867d355ed3c1
Wozniak Klaudia <klaudia.wozniak@inetum.com>
2024-02-16 11:21:46 +0100

    Update CustomerModel

--- src/test/java/models/CustomerModel.java
+++ src/test/java/models/CustomerModel.java
@@ -1,24 +1,30 @@
 package models;
 
 import com.github.javafaker.Faker;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Value;
 
 import java.util.Locale;
+
+@Getter
+@Setter
+@Builder
+@Value
 
 public class CustomerModel {
 
     Faker fakerEN = Faker.instance(new Locale("en-gb"));
-    @Getter
-    @Setter
-    String firstName = fakerEN.name().firstName();
-    String lastName = fakerEN.name().lastName();
-    String email = fakerEN.internet().safeEmailAddress();
-    String address = fakerEN.address().streetAddress();
-    String city = fakerEN.address().city();
-    String state = fakerEN.address().stateAbbr();
-    String zipCode = fakerEN.address().zipCode();
-    private final String countryDefault = "United Kingdom";
-    String login = fakerEN.name().username();
-    String password = fakerEN.internet().password(4, 20);
+
+    public String firstName = fakerEN.name().firstName();
+    public String lastName = fakerEN.name().lastName();
+    public String email = fakerEN.internet().safeEmailAddress();
+    public String address = fakerEN.address().streetAddress();
+    public String city = fakerEN.address().city();
+    public String state = fakerEN.address().stateAbbr();
+    public String zipCode = fakerEN.address().zipCode();
+    public final String countryDefault = "United Kingdom";
+    public String login = fakerEN.name().username();
+    public String password = fakerEN.internet().password(4, 20);
 }
