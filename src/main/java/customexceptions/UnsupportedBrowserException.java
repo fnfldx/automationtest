a8e45afdfe626d5fe24fd2370e19dd2fad847297
Kuba <kuba.krzysiek96@gmail.com>
2024-02-23 15:17:34 +0100

    Removed empty lines from custom exception class

--- src/main/java/customexceptions/UnsupportedBrowserException.java
+++ src/main/java/customexceptions/UnsupportedBrowserException.java
@@ -3,10 +3,7 @@ package customexceptions;
 import enums.BrowserName;
 
 public class UnsupportedBrowserException extends Exception {
-
     public UnsupportedBrowserException(String browserName) {
-
         super(browserName + " - This browser is not supported yet.");
-
     }
 }
