d26552b63fcc7f6011fbd98cc702516c94de10c8
Jakub Krzysiek <kuba.krzysiek96@gmail.com>
2024-02-24 01:05:53 +0100

    Updated Selenium version in pom.xml file / First version of working with singletons

--- src/main/java/engine/property/manager/PropertyManager.java
+++ src/main/java/engine/property/manager/PropertyManager.java
@@ -1,29 +1,48 @@
 package engine.property.manager;
 
+import lombok.Getter;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+@Getter
 public class PropertyManager {
-  public static String getProperty(PropertyKeys key) {
-    var properties = new Properties();
+  private static PropertyManager instance;
+  private static Properties properties;
+
+  static {
+    properties = new Properties();
     var configPath = "src/main/resources/config.properties";
     try (FileInputStream input = new FileInputStream(configPath)) {
-        properties.load(input);
+      properties.load(input);
     } catch (IOException ex) {
-        ex.printStackTrace();
+      ex.printStackTrace();
     }
+  }
+
+  private PropertyManager() {
+    // Private constructor to prevent instantiation from outside
+  }
+
+  public static synchronized PropertyManager getInstance() {
+    if (instance == null) {
+      instance = new PropertyManager();
+    }
+    return instance;
+  }
+
+  public static String getProperty(PropertyKeys key) {
     return properties.getProperty(key.toString());
-    }
+  }
 
-    public static void setProperty(String systemProperty, String systemStringValue) {
-      System.setProperty(systemProperty, systemStringValue);
-    }
-
-    public enum PropertyKeys {
-      CHROME_DRIVER,
-      FIREFOX_DRIVER,
-      BASE_URL,
-      BASE_WAIT_IN_SECONDS
+  public static void setProperty(String systemProperty, String systemStringValue) {
+    System.setProperty(systemProperty, systemStringValue);
+  }
+  public enum PropertyKeys {
+    CHROME_DRIVER,
+    FIREFOX_DRIVER,
+    BASE_URL,
+    BASE_WAIT_IN_SECONDS
   }
 }
