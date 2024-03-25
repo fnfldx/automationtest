7db198d56b1b6fa8ffaadc3ef06db6ce8dd68e7c
TOM-AN1 <tomasz.jarzabek@antnetic.pl>
2024-03-26 00:12:09 +0100

    Revert "modifing MainPageProducts"
    This reverts commit b96b6f68ee3c4d732c8d8fc7d43b9c4e1ec84663.

--- src/main/java/pages/MainPageProducts.java
+++ src/main/java/pages/MainPageProducts.java
@@ -1,26 +1,3 @@
 package pages;
 public class MainPageProducts {
-    private String sectionId;
-    private int productNumber;
-
-    public MainPageProducts(String sectionId, int productNumber) {
-        this.sectionId = sectionId;
-        this.productNumber = productNumber;
-    }
-
-    public String getSectionId() {
-        return sectionId;
-    }
-    public void setSectionId(String sectionId) {
-        this.sectionId = sectionId;
-    }
-
-    public int getProductNumber() {
-        return productNumber;
-    }
-
-    public void setProductNumber(int productNumber) {
-        this.productNumber = productNumber;
-    }
-
 }
