63104fc30a581a9360ccf5aba10baa94860a348e
Kuba <kuba.krzysiek96@gmail.com>
2024-03-27 11:02:35 +0100

    Added LoginPageSteps / small refactor to other relevant classes

---
+++ src/main/java/steps/LoginPageSteps.java
@@ -0,0 +1,23 @@
+package steps;
+
+
+import org.openqa.selenium.By;
+import pages.LoginPage;
+
+import static engine.drivers.WebDriverFactory.getWebDriverInstance;
+
+public class LoginPageSteps {
+
+    protected static LoginPage loginPage;
+
+    public static void proceedAsGuest() {
+        loginPage = new LoginPage();
+        loginPage.selectGuestAccountOption();
+        loginPage.clickContinueButton();
+    }
+
+    public static void goToAccountLogin() {
+        var loginOrRegister = getWebDriverInstance().findElement(By.id("customer_menu_top"));
+        loginOrRegister.click();
+    }
+}
