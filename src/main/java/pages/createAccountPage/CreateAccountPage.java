1f87a966cea01271f7c7fa75bcf5a81049f684bf
fnfldx <kacppod623@student.polsl.pl>
2024-04-08 15:36:46 +0200

    Add CreateAccountForm - enum with input's id

---
+++ src/main/java/pages/createAccountPage/CreateAccountPage.java
@@ -0,0 +1,15 @@
+package pages.createAccountPage;
+
+import enums.NavbarButton;
+import models.CustomerModel;
+import org.openqa.selenium.By;
+import pages.BasePage;
+
+public class CreateAccountPage {
+    protected BasePage basePage;
+    public CustomerModel customerModel;
+
+    public By inputFormLocatorById() {
+        return By.xpath();
+    }
+}
