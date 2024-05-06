66fe20322566518ac9907e62c14799bcee356a11
fnfldx <kacppod623@student.polsl.pl>
2024-05-06 12:48:11 +0200

    Refactor TotalsTable - Locator

--- src/main/java/pages/cartPage/TotalsTable.java
+++ src/main/java/pages/cartPage/TotalsTable.java
@@ -9,7 +9,7 @@ import static enums.Currency.convertCurrencyFromCookie;
 
 public class TotalsTable {
     public final String totalsTable = "//table[@id='totals_table']";
-    public By subTotalLocator = By.xpath(totalsTable + "/tbody/tr[1]/td[2]/span");
+    public By subTotalLocator = By.xpath(totalsTable + "/tbody/tr[1]/td[2]");
     public By flatShippingRateLocator = By.xpath(totalsTable + "/tbody/tr[2]/td[2]");
     public By totalLocator = By.xpath(totalsTable + "/tbody/tr[3]/td[2]");
     protected BasePage basePage;
