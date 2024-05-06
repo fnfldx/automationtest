e29dba73b78afe3c4abc9263f5f0aa4ba593d0ea
fnfldx <kacppod623@student.polsl.pl>
2024-05-06 14:59:24 +0200

    ApplyCouponCodeTable constructor

--- src/main/java/pages/cartPage/ApplyCouponCodeTable.java
+++ src/main/java/pages/cartPage/ApplyCouponCodeTable.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.By;
 import pages.BasePage;
 
 public class ApplyCouponCodeTable {
-
     public By applyCouponCodeTableLocator = By.xpath("//div[contains(@class,'coupon')]/table");
     public By applyCouponCodeFormLocator = By.id("coupon");
     public By couponInputLocator = By.id("coupon_coupon");
     public By applyCouponButtonLocator = By.id("apply_coupon_btn");
     protected BasePage basePage;
+
+    public ApplyCouponCodeTable() {
+        this.basePage = new BasePage();
+    }
 
     void enterCouponCode(String couponCode) {
         basePage.enterTextToElement(couponInputLocator, couponCode);
