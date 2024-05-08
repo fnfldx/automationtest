94681ca503b7858aaf1761e73f011ea28631c18b
Kuba <kuba.krzysiek96@gmail.com>
2024-05-08 09:07:53 +0200

    Added a few new things to OrderConfirmationPage class

--- src/main/java/pages/OrderConfirmationPage.java
+++ src/main/java/pages/OrderConfirmationPage.java
@@ -7,9 +7,25 @@ public class OrderConfirmationPage {
 
     // Locators:
     public By orderConfirmationMessageLocator = By.xpath("//h1[contains(text(), 'YOUR ORDER HAS BEEN PROCESSED')]");
+    public By invoicePageHyperlinkLocator = By.xpath("//*[@id='maincontainer']/div/div/div/div/section/p[3]/a");
+    public By contactWithStoreOwnerrHyperlinkLocator = By.xpath("//*[@id='maincontainer']/div/div/div/div/section/p[4]/a");
+    public By continueButtonLocator = By.xpath("//*[@id='maincontainer']/div/div/div/div/section/a");
+
 
     public OrderConfirmationPage() {
         this.basePage = new BasePage();
+    }
+
+    public void goToInvoicePage() {
+        basePage.clickOnElement(invoicePageHyperlinkLocator);
+    }
+
+    public void goToContactWithStoreOwner() {
+        basePage.clickOnElement(contactWithStoreOwnerrHyperlinkLocator);
+    }
+
+    public void continueShopping() {
+        basePage.clickOnElement(continueButtonLocator);
     }
 
     public boolean isOrderConfirmationMessageVisible() {
