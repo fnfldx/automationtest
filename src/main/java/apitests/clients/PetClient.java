aaf5488cd794b56b20d7591e24095cd59ba23def
krzybrze <krzysztof.brzezinski@inetum.com>
2024-05-09 11:04:57 +0200

    BA-40-setupApiTestEngine code review fix

--- src/main/java/apitests/clients/PetClient.java
+++ src/main/java/apitests/clients/PetClient.java
@@ -1,9 +1,11 @@
 package apitests.clients;
 
 import apitests.model.Pet;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class PetClient extends BaseClient {
 
@@ -12,13 +14,13 @@ public class PetClient extends BaseClient {
     public Response getPetsByStatus(final String status) {
         return given()
                 .when()
-                .queryParams("status", status)
+                .queryParams(Pet.Fields.status, status)
                 .get(PET_URL + "findByStatus");
     }
 
     public Response postPet(final Pet pet) {
         return given()
-                .contentType("application/json")
+                .contentType(JSON)
                 .body(pet)
                 .when()
                 .post(PET_URL);
