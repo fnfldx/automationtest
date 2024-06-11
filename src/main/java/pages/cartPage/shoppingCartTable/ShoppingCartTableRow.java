54fc56877047fe767fd70753ca9e0e31cc1fa301
ep-kapo <Kacper.Podawca@bystronic.com>
2024-06-11 09:16:53 +0200

    import fix

--- src/main/java/pages/cartPage/shoppingCartTable/ShoppingCartTableRow.java
+++ src/main/java/pages/cartPage/shoppingCartTable/ShoppingCartTableRow.java
@@ -4,13 +4,13 @@ import enums.Currency;
 import models.CheckoutProductModel;
 import models.ProductModel;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import pages.BasePage;
+
 import java.math.BigDecimal;
 import java.util.List;
 
-import static engine.drivers.WebDriverFactory.getWebDriverInstance;
+import static enums.Currency.getPriceAsBigDecimal;
 
 public class ShoppingCartTableRow {
     private final String xpath;
