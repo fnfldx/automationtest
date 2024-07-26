5d329a99dcfc07ac96edd90556574502902706f3
ep-kapo <Kacper.Podawca@bystronic.com>
2024-07-26 11:40:05 +0200

    Renaming packages

---
+++ api-pet-store-cucumber/src/main/java/clients/BaseClient.java
@@ -0,0 +1,5 @@
+package clients;
+
+public class BaseClient {
+    public static final String BASE_URL = "https://petstore.swagger.io/v2";
+}
--- apipetstorecucumber/src/main/java/clients/BaseClient.java
+++
@@ -1,5 +0,0 @@
-package clients;
-
-public class BaseClient {
-    static final String BASE_URL = "https://petstore.swagger.io/v2";
-}
